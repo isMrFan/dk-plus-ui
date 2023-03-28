acae77befc497865f7956f9e39d2c5bbc7c70108
北斋 <1038886097@qq.com>
2023-03-28 17:14:55 +0800

    配置修改

---
+++ commitlint.config.ts
@@ -0,0 +1,10 @@
+const commitlintConfig = {
+  /**
+   * 强制执行固定的提交规范
+   *
+   * @see config-conventional https://www.npmjs.com/package/@commitlint/config-conventional
+   */
+  extends: ['@commitlint/config-conventional']
+}
+
+export default commitlintConfig
