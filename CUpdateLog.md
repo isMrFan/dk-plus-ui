88001456e675db10e30dd88dd7af7b8985afadc4
北斋 <1038886097@qq.com>
2023-04-06 10:05:24 +0800

    初始化

--- CUpdateLog.md
+++
@@ -1,6 +0,0 @@
-# 更新日志
-
-中文 | [英文](https://github.com/CadWalaDers/dk-ui/blob/master/CUpdateLog.en-US.md)
-
-等待更新
-按时
--- npm/dist/lib/utils/with-install.js
+++ npm/dist/lib/utils/with-install.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.withInstall = void 0;
 const withInstall = (comp) => {
+    ;
     comp.install = function (app) {
         app.component(comp.name, comp);
     };
