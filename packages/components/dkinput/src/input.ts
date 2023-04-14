f6ecb115d10dfca4593d65a26fe5437f43291bf1
王英杰 <code_yingjie@163.com>
2023-04-14 14:52:25 +0800

    style: 新增input组件默认样式

--- packages/components/dkinput/src/input.ts
+++ packages/components/dkinput/src/input.ts
@@ -1,56 +1,59 @@
 
-import { ExtractPropTypes } from 'vue'
+import { ExtractPropTypes, PropType } from 'vue'
 export const DKinput = {
+  value: {
+    type: [String, Number] as PropType<string | number>,
+  },
   type: {
     type: String
   },
-  // size: {
-  //   type: String
-  // },
-  // disabled: {
-  //   type: Boolean
-  // },
-  // clearable: {
-  //   type: Boolean
-  // },
-  // readonly: {
-  //   type: Boolean
-  // },
-  // placeholder: {
-  //   type: String
-  // },
-  // maxlength: {
-  //   type: [Number, String]
-  // },
-  // minlength: {
-  //   type: [Number, String]
-  // },
-  // showWordLimit: {
-  //   type: Boolean
-  // },
-  // prefixIcon: {
-  //   type: String
-  // },
-  // suffixIcon: {
-  //   type: String
-  // },
-  // rows: {
-  //   type: [Number, String]
-  // },
-  // autosize: {
-  //   type: [Boolean, Object]
-  // },
-  // autocomplete: {
-  //   type: String
-  // },
-  // autoClearSpace: {
-  //   type: Boolean
-  // },
-  // name: {
-  //   type: String
-  // },
-  // validateEvent: {
-  //   type: Boolean
-  // }
+  size: {
+    type: String
+  },
+  disabled: {
+    type: Boolean
+  },
+  clearable: {
+    type: Boolean
+  },
+  readonly: {
+    type: Boolean
+  },
+  placeholder: {
+    type: String
+  },
+  maxlength: {
+    type: [Number, String]
+  },
+  minlength: {
+    type: [Number, String]
+  },
+  showWordLimit: {
+    type: Boolean
+  },
+  prefixIcon: {
+    type: String
+  },
+  suffixIcon: {
+    type: String
+  },
+  rows: {
+    type: [Number, String]
+  },
+  autosize: {
+    type: [Boolean, Object]
+  },
+  autocomplete: {
+    type: String
+  },
+  autoClearSpace: {
+    type: Boolean
+  },
+  name: {
+    type: String
+  },
+  validateEvent: {
+    type: Boolean
+  }
 }
 export type dkinput = ExtractPropTypes<typeof DKinput>
--- play/views/component/DkInput/DkInput.vue
+++ play/views/component/DkInput/DkInput.vue
@@ -1,16 +1,34 @@
 <template>
-  <div class="dk-input">
-    <dk-input />
+  <div class="input">
+    <div>基础用法</div>
+    <dk-input v-model="msg" placeholder="请输入内容" />
+    {{ msg }}
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, watch } from "vue";
 export default defineComponent({
   name: "DkInputComponent",
   setup() {
+    let msg = ref("Hello World");
+    watch(msg, (val) => {
+      console.log(val);
+    });
+    const a = (a) => {
+      console.log(a);
+      
+    }
+    return {
+      msg,
+      a
+    };
   },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.dk-input {
+  width: 180px;
+}
+</style>
