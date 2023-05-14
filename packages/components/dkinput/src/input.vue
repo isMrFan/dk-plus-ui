d45e0282a32d7a11392f22141238177ac5cd7a18
王英杰 <wangyingjie@192.168.0.105>
2023-05-15 00:04:45 +0800

    style: 添加input.clearable样式

--- packages/components/dkinput/src/input.vue
+++ packages/components/dkinput/src/input.vue
@@ -1,66 +1,92 @@
 <script lang="ts">
-/**
- * @name dk-input
- * @author WangYingJie
- * @Time 2323/4/12
- * @description 输入框组件
- * @example
- */
-import { defineComponent, computed, ref, nextTick, Ref, ComputedRef, watch, reactive, toRefs, toRaw } from "vue";
-import { dkInputProps, haInputClass } from "./props";
-import { getInputGlobal } from "../../_hooks";
-import { getInput } from "../../_hooks";
+  /**
+   * @name dk-input
+   * @author WangYingJie
+   * @Time 2323/4/12
+   * @description 输入框组件
+   * @example
+   */
+  import {
+    defineComponent,
+    computed,
+    ref,
+    nextTick,
+    Ref,
+    ComputedRef,
+    watch,
+    reactive,
+    toRefs,
+    toRaw
+  } from 'vue'
+  import { dkInputProps, haInputClass } from './props'
+  import { getInputGlobal } from '../../_hooks'
+  import { getInput } from '../../_hooks'
 
+  export default defineComponent({
+    name: 'DkInput',
+    props: dkInputProps,
+    emit: ['update:modelValue'],
+    setup(props, { slots, emit }) {
+      const { getInputType } = getInputGlobal(props)
+      const { type = getInputType(), placeholder, clearable } = props
 
+      const {
+        classList,
+        styleList,
+        wrapperClassList,
+        innerClassList,
+        clearableClassList
+      } = getInput(props)
 
+      const CLASS_LIST = computed(() => getInput(props).classList)
 
-export default defineComponent({
-  name: "DkInput",
-  props: dkInputProps,
-  emit: ['update:modelValue'],
-  setup(props, { slots, emit }) {
-    const { getInputType } = getInputGlobal(props);
-    const { type = getInputType(), placeholder } = props;
-    
-    const { classList, styleList, wrapperClassList, innerClassList } = getInput(props)
+      const MODEL_VALUE = ref<string | number>(props.modelValue)
 
-    const MODEL_VALUE = ref<string | number>(props.modelValue);
+      const update = (e: Event) => {
+        const TARGET = e.target as HTMLInputElement
+        MODEL_VALUE.value = TARGET.value
+        emit('update:modelValue', MODEL_VALUE.value)
+        console.log(IS_CLEAR.value);
+        
+      }
 
-    const update = (e: Event) => {
-      const TARGET = e.target as HTMLInputElement;
-      MODEL_VALUE.value = TARGET.value;
-      emit('update:modelValue', MODEL_VALUE.value);
-    };
-
-    const DISABLED = computed((): boolean => props.disabled)
-    const PLACEHOLDER = ref<string | number>(placeholder)
-    const INPUT_ATTRS = computed(() => {
-      return {
+      const DISABLED = computed((): boolean => props.disabled)
+      const PLACEHOLDER = ref<string | number>(placeholder)
+      const INPUT_ATTRS = computed(() => {
+        return {
           class: innerClassList.value,
           type,
           placeholder: PLACEHOLDER.value,
           onInput: update,
-          disabled: DISABLED.value,
+          disabled: DISABLED.value
+        }
+      })
+
+      const IS_CLEAR = computed(() => {
+        return !!clearable && !!MODEL_VALUE.value && !DISABLED.value
+      })
+
+      return {
+        classList: CLASS_LIST.value,
+        styleList,
+        wrapperClassList,
+        value: MODEL_VALUE.value,
+        inputAttrs: toRaw(INPUT_ATTRS),
+        isClear: IS_CLEAR
       }
-    })
-    
-    const CLASS_LIST = computed(() => getInput(props).classList)
-    
-    return {
-      classList: CLASS_LIST.value,
-      styleList,
-      wrapperClassList,
-      value: MODEL_VALUE.value,
-      inputAttrs: toRaw(INPUT_ATTRS),
-    };
-  }
-});
+    }
+  })
 </script>
 
 <template>
   <div :class="classList" :style="styleList">
     <div :class="wrapperClassList">
       <input v-model="value" :...="inputAttrs" />
+      <template v-if="isClear">
+        <dk-icon
+        class="dk-icon-del1 dk-input-clearable"
+        />
+      </template>
     </div>
   </div>
   <!-- <div>
