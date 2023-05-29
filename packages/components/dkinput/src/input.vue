1a62312d64b37443db18d21f91157703c62e380f
王英杰 <code_yingjie@163.com>
2023-05-29 10:51:40 +0800

    pref: 精简1.0.1

--- packages/components/dkinput/src/input.vue
+++ packages/components/dkinput/src/input.vue
@@ -5,47 +5,17 @@
  * @Time 2323/4/12
  * @description 输入框组件
  * @function getNull 获取字符串、数组长度
- * @function getBoolean 获取布尔值
+ * @function getBooleanAnd 获取布尔值
  * @example
  */
 import type { InputHTMLAttributes, ComputedRef, TextareaHTMLAttributes } from 'vue';
 import { defineComponent, computed, ref, shallowRef, nextTick, reactive } from 'vue';
 import { dkInputProps } from './props';
 import { getInputGlobal } from '../../_hooks';
-import { getInput } from '../../_hooks';
+import { getInput, getBooleanAnd, getBooleanOr, getNull } from '../../_hooks';
 import type { dkInputType } from '../../_interface';
+import type { propDataModel, dataType } from './type';
 // import type { DkInputProps } from './props'
-
-interface propDataModel {
-  /** 前缀文本 */
-  prependText: string,
-  /** 后缀文本 */
-  appendText: string,
-  placeholder: string,
-  clearable: boolean,
-  showPassword: boolean,
-  prependIcon: string,
-  appendIcon: string,
-  disabledProp: boolean,
-  prefixIcon: string,
-  inputType: dkInputType,
-  suffixIcon: string
-}
-
-interface dataType {
-  isPrepend: boolean,
-  isPrependText: boolean,
-  isAppendTextLen: boolean,
-  isPrefix: boolean,
-  isShowClear: boolean,
-  isClear: boolean,
-  inputType: dkInputType,
-  inputmode: string,
-  isPrefixIcon: boolean,
-  isSuffix: boolean,
-  isSuffixIcon: boolean,
-  isShowPassword: boolean
-}
 
 export default defineComponent({
   name: 'DkInput',
@@ -77,59 +47,6 @@ export default defineComponent({
     })
 
     /**
-     * @name getNull 获取字符串、数组长度
-     * @param {string | array} target 
-     * @returns {boolean}
-     */
-    const getNull = (target: string | [] | number): boolean => {
-      if (Array.isArray(target)) {
-        if (target.length === 0) 
-        return false
-      } else {
-        target = target.toString().trim()
-        if(typeof target === 'string') {
-          if (target.length === 0) 
-          return false
-        }
-      }
-      return true
-    }
-
-    /** 
-     * @name getBoolean 获取布尔值 与
-     * @param {boolean[]} target
-     * @returns {boolean}
-     */
-    const getBoolean = (target: boolean[]): boolean => {
-      if(target.length == 0) return false
-      let i = 0;
-      while (!target[i]) {
-        return false
-      }
-      i++
-      return true
-    }
-
-    /**
-     * @name getBooleanOr 获取或
-     * @param {boolean[]} target
-     * @returns {boolean}
-     */
-    const getBooleanOr = (target: boolean[]): boolean => {
-      if(target.length == 0) return false
-      let i = 0;
-      while (target[i]) {
-        return true
-      }
-      i++
-      return false
-    }
-
-    // const inputType = computed((): dkInputType | ComputedRef<dkInputType> => {
-    //   return type === 'password' ? passwordShowOrHide.value ? 'text' : 'password' : type;
-    // });
-
-    /**
      * @name verifyInputType 获取input类型
      * @returns {dkInputType}
      */
@@ -156,35 +73,33 @@ export default defineComponent({
       let isDisabled = !propData.disabledProp;
       let isTextarea = propData.inputType !== 'textarea';
       let isPassword = propData.inputType !== 'password';
-      return getBoolean([isDisabled, isTextarea, isPassword, isClearable]);
+      return getBooleanAnd([isDisabled, isTextarea, isPassword, isClearable]);
     }
 
     const data = reactive<dataType>({
       inputType: verifyInputType(),
-      isPrepend: getBoolean([getNull(propData.prependText), !!propData.prependIcon]),
+      isPrepend: getBooleanAnd([getNull(propData.prependText), !!propData.prependIcon]),
       isPrependText: getNull(propData.prependText) && !propData.prependIcon,
       isAppendTextLen: getNull(propData.appendText),
       isPrefix: getBooleanOr([!!slots.prefix, !!propData.prefixIcon]),
       isShowClear: getNull(modelValueProp.value),
       isClear: getIsClear(),
       inputmode: type === 'number' ? 'numeric' : 'text',
-      isPrefixIcon: getBoolean([!!propData.prefixIcon]),
+      isPrefixIcon: getBooleanAnd([!!propData.prefixIcon]),
       isSuffix: getBooleanOr([!!slots.suffix, !!propData.suffixIcon]),
-      isSuffixIcon: getBoolean([!!propData.suffixIcon]),
-      isShowPassword: getBoolean([type === 'password', propData.showPassword])
+      isSuffixIcon: getBooleanAnd([!!propData.suffixIcon]),
+      isShowPassword: getBooleanAnd([type === 'password', propData.showPassword]),
+      isPrependIcon: getBooleanAnd([!!propData.prependIcon]),
+      isAppendIcon: getBooleanAnd([!!propData.appendIcon]),
+      isAppend: getBooleanOr([getNull(propData.appendText), !!propData.appendIcon]),
+      isAppendText: getBooleanOr([getNull(propData.appendText), !propData.appendIcon])
     })
     
-    const prependClassList = computed((): string[] => ['dk-input_prepend', 'dk-input_pend']);
+    const prependClassList = (): string[] => ['dk-input_prepend', 'dk-input_pend'];
 
-    const isAppend = computed((): boolean => {
-      return data.isAppendTextLen || !!propData.appendIcon;
-    })
-    const appendClassList = computed((): string[] => ['dk-input_append', 'dk-input_pend']);
-    const isAppendText = computed((): boolean => {
-      return data.isAppendTextLen && !propData.appendIcon;
-    })
+    const appendClassList = (): string[] => ['dk-input_append', 'dk-input_pend'];
 
-    const pendStyleLis = computed(() => getInput(props).pendStyleList);
+    const pendStyleLis = (): {} => getInput(props).pendStyleList;
     const update = (e: Event): void => {
       let updateModelValue = modelValueProp.value
       if (propData.prependText && !propData.prependIcon) {
@@ -258,40 +173,25 @@ export default defineComponent({
       disabled: propData.disabledProp
     } as TextareaHTMLAttributes);
     return {
+      ...propData,
+      ...data,
       classList: inputClassList.value,
       styleList,
       wrapperClassList,
       value: modelValueProp,
       inputAttrs: inputAttrs,
-      isClear: data.isClear,
-      isShowClear: data.isShowClear,
-      isPrefix: data.isPrefix,
-      isPrefixIcon: data.isPrefixIcon,
       prefixIconClass: prefixIconClass(),
-      isSuffix: data.isSuffix,
-      isSuffixIcon: data.isSuffixIcon,
       suffixIconClass: suffixIconClass(),
       clear,
-      input: input,
-      isShowPassword: data.isShowPassword,
+      input,
       togglePassword,
       showPasswordClass: showPasswordClass(),
       textareaAttrs,
-
-      isPrepend: data.isPrepend,
-      prependTextProp: propData.prependText,
-      prependClassList: prependClassList.value,
-      isPrependText: data.isPrependText,
-      isPrependIcon: !!propData.prependIcon,
-      prependIconProp: propData.prependIcon,
-
-      isAppend: isAppend.value,
-      appendTextProp: propData.appendText,
-      appendClassList: appendClassList.value,
-      pendStyleList: pendStyleLis.value,
-      isAppendText: isAppendText.value,
-      isAppendIcon: !!propData.appendIcon,
-      appendIconProp: propData.appendIcon
+      prependClassList: prependClassList(),
+      isAppend: data.isAppend,
+      appendClassList: appendClassList(),
+      pendStyleList: pendStyleLis(),
+      isAppendText: data.isAppendText
     };
   }
 });
@@ -302,8 +202,8 @@ export default defineComponent({
     <!-- append -->
     <template v-if="isPrepend">
       <div :class="prependClassList" :style="pendStyleList">
-        <span v-if="isPrependText">{{ prependTextProp }}</span>
-        <dk-icon v-if="isPrependIcon" :class="prependIconProp"></dk-icon>
+        <span v-if="isPrependText">{{ prependText }}</span>
+        <dk-icon v-if="isPrependIcon" :class="prependIcon"></dk-icon>
       </div>
     </template>
 
@@ -339,8 +239,8 @@ export default defineComponent({
     <!-- prepend -->
     <template v-if="isAppend">
       <div :class="appendClassList" :style="pendStyleList">
-        <span v-if="isAppendText">{{ appendTextProp }}</span>
-        <dk-icon v-if="isAppendIcon" :class="appendIconProp"></dk-icon>
+        <span v-if="isAppendText">{{ appendText }}</span>
+        <dk-icon v-if="isAppendIcon" :class="appendIcon"></dk-icon>
       </div>
     </template>
   </div>
--- packages/components/dkinput/src/type.ts
+++ packages/components/dkinput/src/type.ts
@@ -1,4 +1,5 @@
 import type { Ref, ComputedRef, CSSProperties } from 'vue'
+import type { dkInputType } from '../../_interface';
 export interface UseInputProps {
   value: string | number
   type: string
@@ -84,4 +85,64 @@ export interface UseInputReturn {
   handleInputExceedMouseout?: (e: Event) => void
   handleInputExceedMouseover?: (e: Event) => void
   handleInputExceedContextmenu?: (e: Event) => void
+}
+
+export interface propDataModel {
+  /** 前置文本 */
+  prependText: string
+  /** 后置文本 */
+  appendText: string
+  /** 占位符 */
+  placeholder: string
+  /** 是否显示清除按钮 */
+  clearable: boolean
+  /** 是否显示密码 */
+  showPassword: boolean
+  /** 前置图标 */
+  prependIcon: string
+  /** 后置图标 */
+  appendIcon: string
+  /** 是否禁用 */
+  disabledProp: boolean
+  /** 前缀图标 */
+  prefixIcon: string
+  /** 输入类型 */
+  inputType: dkInputType
+  /** 后缀图标 */
+  suffixIcon: string
+}
+
+export interface dataType {
+  /** 是否有前缀内容 */
+  isPrepend: boolean
+  /** 是否有前缀文本 */
+  isPrependText: boolean
+  /** 是否有后缀文本 */
+  isAppendTextLen: boolean
+  /** 是否有前缀 */
+  isPrefix: boolean
+  /** 是否显示清除按钮 */
+  isShowClear: boolean
+  /** 是否显示清除按钮 */
+  isClear: boolean
+  /** 输入类型 */
+  inputType: dkInputType
+  /** 输入方式 */
+  inputmode: string
+  /** 是否有前缀图标 */
+  isPrefixIcon: boolean
+  /** 是否有后缀 */
+  isSuffix: boolean
+  /** 是否显示后缀图标 */
+  isSuffixIcon: boolean
+  /** 是否显示密码 */
+  isShowPassword: boolean
+  /** 是否有前缀图标 */
+  isPrependIcon: boolean
+  /** 是否有后缀图标 */
+  isAppendIcon: boolean
+  /** 是否有后缀内容 */
+  isAppend: boolean
+  /** 是否有后缀文本 */
+  isAppendText: boolean
 }
