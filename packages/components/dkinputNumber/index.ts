e33348c8a9c36fc55b59a0e947fb787ffbfd8252
北斋 <1038886097@qq.com>
2023-06-20 18:03:30 +0800

    fix: 1.修复打包错误BUG 移交英杰处理

--- packages/components/dkinputNumber/index.ts
+++ packages/components/dkinputNumber/index.ts
@@ -2,5 +2,4 @@ import number from './src/dk-input-number.vue'
 import { withInstall } from '@dk-plus/utils/with-install'
 const dkInputNumber = withInstall(number)
 export { dkInputNumber }
-
 export default dkInputNumber
