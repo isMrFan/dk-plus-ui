3291072728ceee93ce86b2309ce2a2094b68107f
北斋 <1038886097@qq.com>
2023-06-28 10:29:34 +0800

    feat: 1.新增加载指令

---
+++ packages/components/dkloading/src/loading.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+  /**-
+   * @name dk-loading
+   * @author fankai16
+   * @Time 2023/06/28
+   * @description 加载状态组件指令
+   **/
+</script>
+<template>
+  <div class="dk-loading">加载中</div>
+</template>
--- packages/utils/with-install.ts
+++ packages/utils/with-install.ts
@@ -1,4 +1,4 @@
-import type { App, Plugin } from 'vue' // 只导入类型 而不是导入值
+import type { App, Plugin,Directive } from 'vue' // 只导入类型 而不是导入值
 // 类型必须导出 否则生成不了.d.ts
 export type SFCWithInstall<T> = T & Plugin
 export const withInstall = <T>(comp: any) => {
@@ -6,4 +6,21 @@ export const withInstall = <T>(comp: any) => {
     app.component((comp as any).name, comp)
   }
   return comp as SFCWithInstall<T>
+}
+
+/**
+ * 注册自定义指令组件
+ *
+ * @param { Object } main 组件实例
+ * @param { string } name 组件名
+ * @returns { Object } 组件实例
+ */
+export const installDirective = <T extends Directive>(
+  main: T,
+  name: string
+): SFCWithInstall<T> => {
+  (main as SFCWithInstall<T>).install = (app: App): void => {
+    app.directive(name, main as SFCWithInstall<T>)
+  }
+  return main as SFCWithInstall<T>
 }
