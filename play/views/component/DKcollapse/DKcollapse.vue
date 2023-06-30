f0b362b90cbf90cb96c546ad03e8ae9422dc1729
北斋 <1038886097@qq.com>
2023-06-30 18:00:57 +0800

    docs: 1.更新说明文件

---
+++ play/views/component/DKcollapse/DKcollapse.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+  /**
+   * @name DKcollapse
+   * @author fanKai16
+   * @Time 2023/06/30
+   * @description 面布局
+   **/
+  import { defineComponent, reactive, toRefs } from 'vue'
+  export default defineComponent({
+    name: 'DKcollapse',
+    setup() {
+      const state = reactive({
+        name: 'DKcollapse'
+      })
+      return {
+        ...toRefs(state)
+      }
+    }
+  })
+</script>
+<template>
+  <div>折叠面板</div>
+</template>
