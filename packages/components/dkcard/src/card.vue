10011bd4ed446c912c8dc726f8bf99fc976c8f6a
zhudy <484027291@qq.com>
2023-07-04 10:23:43 +0800

    feat: 新增卡片card组件

---
+++ packages/components/dkcard/src/card.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+  /**
+   * @name Dkcard
+   * @author zdy
+   * @Time 2023/07/04
+   * @description 卡片--通用容器
+   **/
+  import { defineComponent } from 'vue'
+  import { cardProps } from './props'
+  import { getStyleList } from '../../_hooks'
+  export default defineComponent({
+    name: 'DkCard',
+    props: cardProps,
+    setup(props) {
+      const { styles } = getStyleList(props, 'header')
+      const style_List = styles(['height', 'padding'])
+      return { style_List }
+    }
+  })
+</script>
+<template>
+  <div class="dk-card" :style="style_List">
+    <slot />
+  </div>
+</template>
