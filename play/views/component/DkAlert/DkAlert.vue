e98d29d945519aef58d0a0491c0e9bde5b2237a3
zhudy <484027291@qq.com>
2023-07-06 17:48:11 +0800

    feat: 新增DK-Alert组件目录

---
+++ play/views/component/DkAlert/DkAlert.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  export default defineComponent({
+    name: 'DkAlert',
+    setup() {
+   
+      return {
+      
+      }
+    }
+  })
+</script>
+<template>
+  <div>
+    <h2>Dk-Alert 组件</h2>
+    <p>基础用法</p>
+   
+  </div>
+</template>
+<style lang="scss" scoped>
+</style>
