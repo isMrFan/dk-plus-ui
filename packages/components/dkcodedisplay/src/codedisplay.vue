4fd7eeb695379dff131289f0c7dbe954db59b197
bugfix2020 <ts02315607@gmail.com>
2023-07-07 17:52:32 +0800

    style: 更新全局dkcodedisplay按钮样式

--- packages/components/dkcodedisplay/src/codedisplay.vue
+++ packages/components/dkcodedisplay/src/codedisplay.vue
@@ -1,35 +1,35 @@
 <script lang="ts">
-  import { computed, defineComponent, reactive, toRefs } from 'vue'
-  import { CodeDisplay } from './codedisplay'
-  export default defineComponent({
-    name: 'Dkcodedisplay',
-    props: CodeDisplay,
-    setup() {
-      const data = reactive({
-        open: false,
-        height: 0 // 展开的高度
-      })
-      const handleOpenCode = (e: Event): void => {
-        const TARGET = e.target as HTMLElement
-        const TARGET_PARENT = TARGET.parentElement as HTMLElement
-        const TARGET_PARENT_PARENT = TARGET_PARENT.parentElement as HTMLElement
-        const { clientHeight } = TARGET_PARENT_PARENT.querySelector(
-          '.dkcodedisplay_code'
-        )?.querySelector('.language-html') || { clientHeight: 0 }
-        data.height = clientHeight
-        data.open = !data.open
-      }
-      const codeHeight = computed(() => {
-        return data.open ? data.height + 20 + 'px' : '0px'
-      })
-
-      return {
-        ...toRefs(data),
-        codeHeight,
-        handleOpenCode
-      }
+import { computed, defineComponent, reactive, toRefs } from 'vue'
+import { CodeDisplay } from './codedisplay'
+export default defineComponent({
+  name: 'Dkcodedisplay',
+  props: CodeDisplay,
+  setup() {
+    const data = reactive({
+      open: false,
+      height: 0 // 展开的高度
+    })
+    const handleOpenCode = (e: Event): void => {
+      const TARGET = e.target as HTMLElement
+      const TARGET_PARENT = TARGET.parentElement as HTMLElement
+      const TARGET_PARENT_PARENT = TARGET_PARENT.parentElement as HTMLElement
+      const { clientHeight } = TARGET_PARENT_PARENT.querySelector(
+        '.dkcodedisplay_code'
+      )?.querySelector('.language-html') || { clientHeight: 0 }
+      data.height = clientHeight
+      data.open = !data.open
     }
-  })
+    const codeHeight = computed(() => {
+      return data.open ? data.height + 20 + 'px' : '0px'
+    })
+
+    return {
+      ...toRefs(data),
+      codeHeight,
+      handleOpenCode
+    }
+  }
+})
 </script>
 <template>
   <div class="dkcodedisplay">
@@ -39,7 +39,7 @@
         <slot />
       </div>
       <div class="dkcodedisplay_open">
-        <span @click="handleOpenCode">{{ open ? '关闭' : '开启' }}</span>
+        <span @click="handleOpenCode">{{ open ? '关闭' : '展开' }}</span>
       </div>
     </dk-shadow>
   </div>
