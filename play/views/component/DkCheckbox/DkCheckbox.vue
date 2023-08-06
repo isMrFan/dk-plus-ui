4df739a8a87a5dd64987a5198ebe452b45bf3eef
WangYingJay <code_yingjie@163.com>
2023-08-06 21:00:44 +0800

    docs:  贡献者名单

--- play/views/component/DkCheckbox/DkCheckbox.vue
+++ play/views/component/DkCheckbox/DkCheckbox.vue
@@ -14,9 +14,14 @@
   })
 </script>
 <template>
-  <div class="checkbox">
-    <dk-checkbox @change="handleChange">常规使用</dk-checkbox>
-    <dk-checkbox :indeterminate="isIndeterMinate" @change="handleChange">中间状态</dk-checkbox>
+  <div class="box">
+    <h4>基础样式</h4>
+    <div class="checkbox">
+      <dk-checkbox>常规使用</dk-checkbox>
+    </div>
+  </div>
+  <div class="box">
+    <dk-checkbox :indeterminate="isIndeterMinate">中间状态</dk-checkbox>
     <dk-checkbox disabled @change="handleChange">禁用状态</dk-checkbox>
   </div>
 </template>
