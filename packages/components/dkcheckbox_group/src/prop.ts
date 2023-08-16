15c75b95c6682302453a9d74451d40b1dd7f4d7c
WangYingJay <code_yingjie@163.com>
2023-08-16 20:54:43 +0800

    perfect: checkboxgroup

--- packages/components/dkcheckbox_group/src/prop.ts
+++ packages/components/dkcheckbox_group/src/prop.ts
@@ -1,5 +1,8 @@
+import type { ExtractPropTypes } from 'vue'
 import { setArrayProps } from '../../_utils'
 
 export const checkboxGroup = {
   modelValue: setArrayProps()
 }
+
+export type CheckboxGroupPropsType = ExtractPropTypes<typeof checkboxGroup>
