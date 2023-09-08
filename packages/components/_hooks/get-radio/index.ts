e6350c0280fc9421e66256caa1911dbf278696cb
WangYingJay <code_yingjie@163.com>
2023-09-08 20:38:34 +0800

    style: radio悬浮样式

--- packages/components/_hooks/get-radio/index.ts
+++ packages/components/_hooks/get-radio/index.ts
@@ -1,7 +1,7 @@
 import type { DkRadioType } from '../../dkradio/src/props'
 import { toRaw, type ComputedRef, type CSSProperties } from 'vue'
 import type { ClassListName } from '../../_interface'
-import { getStyleList } from '..'
+import { getColor, getStyleList } from '..'
 
 interface RadioReturnsType {
   classList: ComputedRef<ClassListName>
@@ -29,7 +29,8 @@ export const getRadio = (props: DkRadioType): RadioReturnsType => {
     const style: CSSProperties = {
       '--radio-size': targetSize[size] || targetSize['small'],
       '--radio-checked-color': checkedColor || '#409eff',
-      '--radio-unchecked-color': uncheckedColor || '#dcdfe6'
+      '--radio-unchecked-color': uncheckedColor || '#dcdfe6',
+      '--radio-hover-color': getColor(checkedColor || '#dcdfe6').getDeepen(0.1)
     }
     return {
       ...style
