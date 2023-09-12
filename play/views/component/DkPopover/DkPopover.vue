4c46a124515569af657d23712d96dcd7e9ae01db
WangYingJay <code_yingjie@163.com>
2023-09-12 19:05:59 +0800

    feat: 新增dk-popover 弹出框组件

---
+++ play/views/component/DkPopover/DkPopover.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  export default defineComponent({
+    name: 'DkPopoverComp',
+    setup() {
+      return {}
+    }
+  })
+</script>
+<template>
+  <!-- <div class="container">
+  </div> -->
+  <div class="box" style="margin-left: 200px">
+    <h4>基本使用</h4>
+    <dk-popover>
+      <template #popup>
+        <div>popup</div>
+        <p>我是弹出的内容 </p>
+      </template>
+      <template #default> aaa </template>
+    </dk-popover>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
