10ca88b5a74d2ffa841d288bf1af7fbfffef9fac
北斋 <1038886097@qq.com>
2023-12-14 18:04:12 +0800

    ci: 更新rate1.0

--- packages/components/dkrate/src/rate.vue
+++ packages/components/dkrate/src/rate.vue
@@ -1,4 +1,8 @@
 <script lang="ts">
+  interface markListJson {
+    icon: string
+    modelValue: boolean
+  }
   import { defineComponent, reactive, toRefs, watch } from 'vue'
   import { dkRateProps } from './props'
   export default defineComponent({
@@ -9,10 +13,22 @@
       const data = reactive({
         modelValue: props.modelValue,
         numberValue: props.numberValue,
+        IsNumberValue: 0,
         icon: 'IconStar',
-        isMouseEnter: false
+        isMouseEnter: false,
+        markList: [] as markListJson[]
       })
-      console.log('data.numberValue', data.numberValue)
+      if (typeof data.numberValue === 'string') {
+        data.IsNumberValue = Number(data.numberValue)
+      } else {
+        data.IsNumberValue = data.numberValue
+      }
+      for (let index = 0; index < data.IsNumberValue; index++) {
+        data.markList.push({
+          icon: 'IconStar',
+          modelValue: false
+        })
+      }
       const methods = {
         handleMouseEnter: (): void => {
           data.isMouseEnter = true
@@ -23,8 +39,24 @@
           data.isMouseEnter = false
           data.icon = 'IconStar'
         },
-        handleClick: (): void => {
-          data.icon = 'IconStar'
+        handleClick: (item: markListJson, ind: number): void => {
+          item.modelValue = !item.modelValue
+          if (item.modelValue) {
+            for (let index = 0; index < ind; index++) {
+              data.markList[index].modelValue = true
+              data.markList[index].icon = data.markList[index].modelValue
+                ? 'IconStared'
+                : 'IconStar'
+            }
+          } else {
+            for (let index = ind; index < data.markList.length; index++) {
+              data.markList[index].modelValue = false
+              data.markList[index].icon = data.markList[index].modelValue
+                ? 'IconStared'
+                : 'IconStar'
+            }
+          }
+          item.icon = item.modelValue ? 'IconStared' : 'IconStar'
           emit('update:modelValue', !data.modelValue)
         },
         handleState: (): void => {
@@ -39,7 +71,6 @@
             data.isMouseEnter = false
             // data.icon = 'IconStar'
           }
-          console.log('props.modelValue', val)
           data.modelValue = val
           methods.handleState()
         },
@@ -47,7 +78,6 @@
           immediate: true
         }
       )
-
       return {
         ...toRefs(data),
         ...methods
@@ -56,12 +86,14 @@
   })
 </script>
 <template>
-  <div
-    class="dk-rate"
-    @mouseenter="handleMouseEnter"
-    @mouseleave="handleMouseLeave"
-    @click="handleClick"
-  >
-    <dk-icon v-for="(item, ind) in numberValue" :key="ind" :icon="icon"></dk-icon>
+  <div class="dk-rate">
+    <dk-icon
+      v-for="(item, ind) in markList"
+      :key="ind"
+      :icon="item.icon"
+      @mouseenter="handleMouseEnter()"
+      @mouseleave="handleMouseLeave()"
+      @click="handleClick(item, ind)"
+    ></dk-icon>
   </div>
 </template>
