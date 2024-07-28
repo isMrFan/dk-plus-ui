b148f084d953b1354112d86562023cc19239ac12
Mr. Fan <dk-plus-ui@foxmail.com>
2024-07-28 16:55:43 +0800

    docs: 修改文档增加SEO相关

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,5 +1,6 @@
 import type { UserConfigExport } from 'vite'
 import { resolve } from 'path'
+import { copyFileSync } from 'fs'
 export default (): UserConfigExport => {
   return {
     server: {
@@ -13,6 +14,31 @@ export default (): UserConfigExport => {
     optimizeDeps: {
       exclude: ['vitepress'],
       include: ['vue', '@vueuse/core']
+    },
+    plugins: [
+      {
+      name: 'copy-static-files',
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      buildEnd() {
+        try {
+          // 复制 robots.txt
+          copyFileSync(
+            resolve(__dirname, './robots.txt'),
+            resolve(__dirname, './.vitepress/dist/robots.txt')
+          );
+
+          // 复制 sitemap.xml
+          copyFileSync(
+            resolve(__dirname, './sitemap.xml'),
+            resolve(__dirname, './.vitepress/dist/sitemap.xml')
+          );
+
+          console.log('Static files copied successfully!');
+        } catch (error) {
+          console.error('Error copying static files:', error);
+        }
+      }
     }
+    ]
   }
 }
