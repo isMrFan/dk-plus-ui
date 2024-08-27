1afc4e392f69b90a4d525246ed22e196f5ee3e99
北斋 <1038886097@qq.com>
2024-08-27 10:39:13 +0800

    docs: 修改仓库路径

---
+++ play/views/component/DKSwatches/DKSwatches.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'DKSwatches',
+  setup() { 
+    const htmlJson = {}
+    return {
+      htmlJson
+    }
+  }
+ })
+</script>
+<template>
+  <div class="index_DKSwatches">
+    123
+  </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
